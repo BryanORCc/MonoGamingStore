12b1b58ce1f4107208e139158daf2e3fac27f520
Jorge Gamero <jorge_gd20@hotmail.com>
2021-04-21 03:02:34 -0500

    Creación y diseño Contáctanos

--- MonoGamingStore/Controllers/HomeController.cs
+++ MonoGamingStore/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace MonoGamingStore.Controllers
         {
             return View();
         }
+        public IActionResult Contactanos()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
