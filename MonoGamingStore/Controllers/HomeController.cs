a12906870aeb26ab0bac1fc38e81c046a1c4c98b
Bryan Rodriguez <bryanorcc@outlook.com>
2021-04-21 16:09:48 -0500

    Merge de General al Main

--- MonoGamingStore/Controllers/HomeController.cs
+++ MonoGamingStore/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace MonoGamingStore.Controllers
         {
             return View();
         }
+        public IActionResult Contactanos()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
