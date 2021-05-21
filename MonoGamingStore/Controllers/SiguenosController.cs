2b9384a0007e788b753dcecca815513dc2a7e7dd
Jorge Gamero <jorge_gd20@hotmail.com>
2021-05-20 20:40:39 -0500

    Controladores logger

--- MonoGamingStore/Controllers/SiguenosController.cs
+++ MonoGamingStore/Controllers/SiguenosController.cs
@@ -1,9 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace MonoGamingStore.Controllers
 {
     public class SiguenosController : Controller
     {
+        private readonly ILogger<SiguenosController> _logger;
+
+        public SiguenosController(ILogger<SiguenosController> logger)
+        {
+            _logger = logger;
+        }
         public IActionResult Index(){
             return View();
         }
