3ccc43e6b2868c07d78f31e85364850ec71c9c7e
Bryan Rodriguez <bryanorcc@outlook.com>
2021-05-28 21:22:19 -0500

    Prueba de carga de datos a la bd

--- MonoGamingStore/Startup.cs
+++ MonoGamingStore/Startup.cs
@@ -27,9 +27,7 @@ namespace MonoGamingStore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(Configuration.GetConnectionString("PostgressConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
